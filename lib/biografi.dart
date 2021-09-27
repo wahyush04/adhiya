0fb458fbdb8e1bbda65a575ffe475b7d34483ec1
wahyush04 <wahyush04@gmail.com>
2021-09-27 21:03:08 +0800

    pengarang

--- lib/biografi.dart
+++ lib/biografi.dart
@@ -26,71 +26,103 @@ class Biografi extends StatelessWidget {
                 ],
               ),
             ),
-            Container(
-              height: 400, //height of TabBarView
-              decoration: BoxDecoration(
-                border: Border(top: BorderSide(color: Colors.grey, width: 0.5))
+            SingleChildScrollView(
+              child: Container(
+                height: 500, //height of TabBarView
+                decoration: BoxDecoration(
+                  border: Border(top: BorderSide(color: Colors.grey, width: 0.5))
+                ),
+                child: TabBarView(children: < Widget > [
+                  Container(
+                    child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                    child: Row(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Expanded(child: Text('Maulana Syaikh Tuan Guru Kyai Haji Muhammad Abdul Majid lahir di Bermi, Pancor Kecamatan Selong Kabupaten Lombok timur Provinsi Busa Tenggara Barat', textAlign: TextAlign.justify, style: TextStyle(fontSize: 16), ), flex: 3, ),
+                                        Expanded(child: Padding(
+                                          padding: const EdgeInsets.fromLTRB(15, 0, 0, 0),
+                                            child: Image.asset('assets/tgkh.jpeg'),
+                                        ), flex: 2, )
+                                      ],
+                                    ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                    child: Row(
+                                      children: [
+                                        Expanded(child: Text('TGKH. Muhammad Abdul Majid lahir pada tanggal 20 April 1908 dan meninggal 21 Oktober 1997 pada umur 89 tahun.', textAlign: TextAlign.justify, style: TextStyle(fontSize: 16), ))
+                                      ],
+                                    ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                    child: Row(
+                                      children: [
+                                        Expanded(child: Text('TGKH. Muhammad Abdul Majid seorang ulama besar dari Pulau Lombok dan merupakan pendiri Nahdatul Wathan Diniyah Islamiyah (NWDI), yang merupakan organisasi besar islam di Pulau Lombok', textAlign: TextAlign.justify, style: TextStyle(fontSize: 16), ))
+                                      ],
+                                    ),
+                                )
+                              ],
+                            ),
+                        ),
+                  ),
+                  Container(
+                      child: SingleChildScrollView(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                    child: Row(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Expanded(child: Text('al-Habib Umar bin Muhammad bin Salim bin Hafidz yang dilahirkan pada hari Senin, 27 Mei 1963 M. adalah seorang ulama dunia era modern. al-Habib ‘Umar kini tinggal di Tarim, Yaman di mana dia mengawasi perkembangan di Dar-al Musthafa dan berbagai sekolah lain yang telah dibangun di bawah manajemennya. ', textAlign: TextAlign.justify, style: TextStyle(fontSize: 16), ), flex: 3, ),
+                                        Expanded(child: Padding(
+                                          padding: const EdgeInsets.fromLTRB(15, 0, 0, 0),
+                                            child: Image.asset('assets/Habib_Umar.jpg'),
+                                        ), flex: 2, )
+                                      ],
+                                    ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                    child: Row(
+                                      children: [
+                                        Expanded(child: Text('Dia masih memegang peran aktif dalam dakwah agama Islam, sedemikian aktifnya sehingga dia meluangkan hampir sepanjang tahunnya mengunjungi berbagai negara di seluruh dunia demi melakukan kegiatan-kegiatan mulianya itu.', textAlign: TextAlign.justify, style: TextStyle(fontSize: 16), ))
+                                      ],
+                                    ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                    child: Row(
+                                      children: [
+                                        Expanded(child: Text("Ada banyak karangan kitab al habib umar bin muhammmad bin salim bin hafizd salah satu yang terkenal di Indonesia adalah Dhiyaullami'bidzikri Maulid an-Nabi asy-Syafi atau dikenal dengan sebutan adhiya ullami. Kitab Maulid Adh-Dhiyaul Lami’ merupakan kitab yang disusun oleh Al-Hafidh Al-Musnid Al-Habib Umar bin Muhammad bin Salim bin Hafidh Maulid AdhDhiyaul lami’ ditulis pada tahun 1994 di kota Syihir, dekat Mukalla, Hadramaut, saat saya disana. Maulid Adhdhiya'ullami ini ditulis oleh Guru Mulia pada saat dini hari dan rampungnya pada saat sebelum akhir sepertiga malam terakhir", textAlign: TextAlign.justify, style: TextStyle(fontSize: 16), ))
+                                      ],
+                                    ),
+                                )
+                              ],
+                            ),
+                        ),
+                      )
+                    ),
+                  // Container(
+                  //   child: Center(
+                  //     child: Text('Display Tab 3', style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+                  //   ),
+                  // ),
+                  // Container(
+                  //   child: Center(
+                  //     child: Text('Display Tab 4', style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+                  //   ),
+                  // ),
+                ])
               ),
-              child: TabBarView(children: < Widget > [
-                Container(
-                  child: Container(
-                    child: SingleChildScrollView(
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                          child: Column(
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                  child: Row(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: [
-                                      Expanded(child: Text('Maulana Syaikh Tuan Guru Kyai Haji Muhammad Abdul Majid lahir di Bermi, Pancor Kecamatan Selong Kabupaten Lombok timur Provinsi Busa Tenggara Barat', textAlign: TextAlign.justify, style: TextStyle(fontSize: 16), ), flex: 3, ),
-                                      Expanded(child: Padding(
-                                        padding: const EdgeInsets.fromLTRB(15, 0, 0, 0),
-                                          child: Image.asset('assets/tgkh.jpeg'),
-                                      ), flex: 2, )
-                                    ],
-                                  ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                  child: Row(
-                                    children: [
-                                      Expanded(child: Text('TGKH. Muhammad Abdul Majid lahir pada tanggal 20 April 1908 dan meninggal 21 Oktober 1997 pada umur 89 tahun.', textAlign: TextAlign.justify, style: TextStyle(fontSize: 16), ))
-                                    ],
-                                  ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                  child: Row(
-                                    children: [
-                                      Expanded(child: Text('TGKH. Muhammad Abdul Majid seorang ulama besar dari Pulau Lombok dan merupakan pendiri Nahdatul Wathan Diniyah Islamiyah (NWDI), yang merupakan organisasi besar islam di Pulau Lombok', textAlign: TextAlign.justify, style: TextStyle(fontSize: 16), ))
-                                    ],
-                                  ),
-                              )
-                            ],
-                          ),
-                      ),
-                    )
-
-                  ),
-                ),
-                Container(
-                  child: Center(
-                    child: Text('Pengarang Kitab', style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
-                  ),
-                ),
-                // Container(
-                //   child: Center(
-                //     child: Text('Display Tab 3', style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
-                //   ),
-                // ),
-                // Container(
-                //   child: Center(
-                //     child: Text('Display Tab 4', style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
-                //   ),
-                // ),
-              ])
             )
           ])
         ),
