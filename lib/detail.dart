65c929b4eff6f799e7c7d234baa517bc9f2794c9
wahyush04 <wahyush04@gmail.com>
2021-09-27 18:17:59 +0800

    audio

--- lib/detail.dart
+++ lib/detail.dart
@@ -1,3 +1,4 @@
+
 import 'package:adhiya/data/data.dart';
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/cupertino.dart';
@@ -31,6 +32,19 @@ class _DetailState extends State < Detail > {
   AudioCache audioCache = AudioCache();
 
   // String path = 'audio/lofi.mp3';
+  int timeProgress = 0;
+  int audioDuration = 0;
+
+  Widget slider(){
+    return Slider.adaptive(
+        value: (timeProgress / 1000).floorToDouble(),
+        max: ((audioDuration / 1000.floorToDouble())), 
+        activeColor: Colors.blue,
+        inactiveColor: Colors.blueGrey,
+        onChanged: (value){
+          seekToSec(value.toInt());
+        });
+  }
 
   @override
   void initState() {
@@ -40,6 +54,11 @@ class _DetailState extends State < Detail > {
     audioPlayer.onPlayerStateChanged.listen((PlayerState s) {
       setState(() {
         audioPlayerState = s;
+      });
+    });
+    audioPlayer.onAudioPositionChanged.listen((Duration p) {
+      setState(() {
+        timeProgress = p.inMilliseconds;
       });
     });
   }
@@ -61,6 +80,62 @@ class _DetailState extends State < Detail > {
     await audioPlayer.pause();
   }
 
+  void seekToSec(int sec) {
+    Duration newPosition = Duration(seconds: sec);
+    audioPlayer.seek(newPosition);
+  }
+
+  // Widget getFileAudioDuration(){
+  //   return FutureBuilder(future: _getAudioDuration(), builder: (BuildContext context, Asy),);
+  // }
+
+  Future<int> _getAudioDuration() async {
+    Uri audioFile = await audioCache.load(DataAdhiyaList[widget.i].audio);
+    await audioPlayer.setUrl(
+      audioFile.path,
+    );
+
+    audioDuration = await Future.delayed(
+      Duration(seconds: 2),
+      () => audioPlayer.getDuration(),
+    );
+
+    return audioDuration;
+  }
+
+
+  Widget getLocalFileDuration() {
+    return FutureBuilder(
+      future: _getAudioDuration(),
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.none:
+            return Text('No Connection...');
+          case ConnectionState.active:
+          case ConnectionState.waiting:
+            return Text('Waiting...');
+          case ConnectionState.done:
+            if (snapshot.hasError) return Text('Error: ${snapshot.error}');
+            return Text(getTimeString(snapshot.data));
+        }
+
+          // if (snapshot.connectionState == ConnectionState.done){
+          //   return Text(getTimeString(snapshot.data));
+          // }
+          // return Text('');
+      },
+    );
+  }
+
+  String getTimeString(int milliseconds) {
+    if (milliseconds == null) milliseconds = 0;
+    String minutes =
+        '${(milliseconds / 60000).floor() < 10 ? 0 : ''}${(milliseconds / 60000).floor()}';
+    String seconds =
+        '${(milliseconds / 1000).floor() % 60 < 10 ? 0 : ''}${(milliseconds / 1000).floor() % 60}';
+    return '$minutes:$seconds'; // Returns a string with the format mm:ss
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -72,12 +147,6 @@ class _DetailState extends State < Detail > {
           },
           child: Icon(Icons.chevron_left)),
         title: Text("Adhiya Ullami"),
-        // actions: < Widget > [
-        //   IconButton(onPressed: () {
-        //     ScaffoldMessenger.of(context).showSnackBar(
-        //       const SnackBar(content: Text('This is a snackbar')));
-        //   }, icon: Icon(Icons.play_arrow_rounded))
-        // ],
       ),
       body: SafeArea(
         child: SingleChildScrollView(
@@ -132,43 +201,75 @@ class _DetailState extends State < Detail > {
           ),
         ),
       ),
-      bottomNavigationBar: BottomAppBar(
+      bottomNavigationBar: Container(
+        height: 100,
         color: Colors.green,
         child: Padding(
-          padding: const EdgeInsets.all(5.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: < Widget > [
-                IconButton(onPressed: () => Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) {
-                    int index = widget.i;
-                    int iback = widget.i - 1;
-                    if (index == 0) {
-                      return Detail(i: index);
-                    } else {
-                      return Detail(i: iback);
-                    }
-                    // return Detail(i: iback);
-                  })),
-                  icon: Icon(Icons.skip_previous)),
-                IconButton(
-                  onPressed: () => audioPlayerState == PlayerState.PLAYING ? pauseAudio() : playAudio(),
-                  icon: Icon(audioPlayerState == PlayerState.PLAYING ? Icons.pause_rounded : Icons.play_arrow_rounded)),
-                IconButton(onPressed: () => Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) {
-                    int index = widget.i;
-                    int inext = widget.i + 1;
-                    if (index == 11) {
-                      return Detail(i: index);
-                    } else {
-                      return Detail(i: inext);
-                    }
-                    // return Detail(i: iback);
-                  })),
-                  icon: Icon(Icons.skip_next)),
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Expanded(
+                flex: 1,
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(getTimeString(timeProgress)),
+                      slider(),
+                      audioDuration == 0? getLocalFileDuration():Text(getTimeString(audioDuration))
+                      // Expanded(flex: 1,child: Text(getTimeString(timeProgress))),
+                      // Expanded(flex: 5,child: slider()),
+                      // Expanded(flex: 1,child: audioDuration == 0? getLocalFileDuration():Text(getTimeString(audioDuration)))
+                    ],
+                  ),
+              ),
+              Expanded(
+                flex: 2,
+                child: BottomAppBar(
+                color: Colors.green,
+                child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: < Widget > [
+                  IconButton(onPressed: () => Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) {
+                      pauseAudio();
+                      int index = widget.i;
+                      int iback = widget.i - 1;
+                      if (index == 0) {
+                        return Detail(i: index);
+                      } else {
+                        return Detail(i: iback);
+                      }
+                      // return Detail(i: iback);
+                    })),
+                    icon: Icon(Icons.skip_previous)),
+                  IconButton(
+                    onPressed: () => audioPlayerState == PlayerState.PLAYING ? pauseAudio() : playAudio(),
+                    icon: Icon(audioPlayerState == PlayerState.PLAYING ? Icons.pause_rounded : Icons.play_arrow_rounded)),
+                  IconButton(onPressed: () => Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) {
+                      pauseAudio();
+                      int index = widget.i;
+                      int inext = widget.i + 1;
+                      if (index == 11) {
+                        return Detail(i: index);
+                      } else {
+                        return Detail(i: inext);
+                      }
+                      // return Detail(i: iback);
+                    })),
+                    icon: Icon(Icons.skip_next)),
 
-              ]
-
-            ),
-        )),
+                ]
+            ),),
+              ),
+            ],
+          ),
+        )
+      )
     );
-  }
+  }  
 }
+
+
+
+
+
