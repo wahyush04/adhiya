f3ceb1ebd91cb9fab28698f4213db328958153db
wahyush04 <wahyush04@gmail.com>
2021-09-28 21:33:46 +0800

    tentang

--- lib/tentang.dart
+++ lib/tentang.dart
@@ -7,7 +7,7 @@ class Tentang extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
+      body: SingleChildScrollView(
         child: Column(
           children: <Widget>[
             Row(
@@ -16,18 +16,29 @@ class Tentang extends StatelessWidget {
                 Padding(
                   padding: const EdgeInsets.all(15.0),
                   child: Image.asset("assets/logo.png",
-                  width: 150, height: 150,),
+                  width: 120, height: 120,),
                 )
               ],
             ),
             Text('Kitab Adhiya Ullami untuk Android 1.0', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
             Padding(
               padding: const EdgeInsets.all(8.0),
-              child: Text('Kitab Adhiya Ullami Digital adalah sebuah piranti lunak yang dapat digunakan untuk membaca kitab Adhiya Ullami yang berisi Shalawat dan Maulid Nabi.',
+              child: Text('Didalam APlikasi Adhiya Ullami versi NWDI memiliki fitur seperti teks arab beserta terjemahannya. Selain itu, aplikasi ini memiliki fitur berupa suara untuk menambah daya tarik pada pengguna.',
               textAlign: TextAlign.justify,
               style: TextStyle(fontSize: 20),),
             ),
-            Text('Kitab Adhiya Ullami Digital')
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text('Aplikasi ini juga dilengkapi dengan bacaan sholawat dan doa, disertai biografi tentang pendiri NWDI dan pengarang kitab Adhiya Ullami.',
+              textAlign: TextAlign.justify,
+              style: TextStyle(fontSize: 20),),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text('Bacaan pada aplikasi ini memiliki 2 kategori, yaitu bacaan secara pilihan dan bacaan full.',
+              textAlign: TextAlign.justify,
+              style: TextStyle(fontSize: 20),),
+            ),
 
           ],),
       ),
