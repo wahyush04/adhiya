f741f1171e07afb496e87f8cec30e13a76a23545
wahyush04 <wahyush04@gmail.com>
2021-09-28 20:50:06 +0800

    audio full

--- lib/Full.dart
+++ lib/Full.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:adhiya/data/data.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 
 class Full extends StatefulWidget {
@@ -12,9 +13,120 @@ class Full extends StatefulWidget {
 }
 
 class _FullState extends State < Full > {
+  AudioPlayer audioPlayer = AudioPlayer();
+  PlayerState audioPlayerState = PlayerState.PAUSED;
+  // CacheAudioPlayer audioCache = CacheAudioPlayer();
+  AudioCache audioCache = AudioCache();
+
+  String path = 'audio/full_bacaan.mp3';
+  int timeProgress = 0;
+  int audioDuration = 0;
+
+  Widget slider(){
+    return Slider.adaptive(
+        value: (timeProgress / 1000).floorToDouble(),
+        max: ((audioDuration / 1000.floorToDouble())), 
+        activeColor: Colors.blue,
+        inactiveColor: Colors.blueGrey,
+        onChanged: (value){
+          seekToSec(value.toInt());
+        });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    audioCache = AudioCache(fixedPlayer: audioPlayer);
+    audioPlayer.onPlayerStateChanged.listen((PlayerState s) {
+      setState(() {
+        audioPlayerState = s;
+      });
+    });
+    audioPlayer.onAudioPositionChanged.listen((Duration p) {
+      setState(() {
+        timeProgress = p.inMilliseconds;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    audioPlayer.release();
+    audioPlayer.dispose();
+    audioCache.clearAll();
+
+  }
+
+  playAudio() async {
+    await audioCache.play(path);
+  }
+
+  pauseAudio() async {
+    await audioPlayer.pause();
+  }
+
+  void seekToSec(int sec) {
+    Duration newPosition = Duration(seconds: sec);
+    audioPlayer.seek(newPosition);
+  }
+
+  // Widget getFileAudioDuration(){
+  //   return FutureBuilder(future: _getAudioDuration(), builder: (BuildContext context, Asy),);
+  // }
+
+  Future<int> _getAudioDuration() async {
+    Uri audioFile = await audioCache.load(path);
+    await audioPlayer.setUrl(
+      audioFile.path,
+    );
+
+    audioDuration = await Future.delayed(
+      Duration(seconds: 2),
+      () => audioPlayer.getDuration(),
+    );
+
+    return audioDuration;
+  }
+
+
+  Widget getLocalFileDuration() {
+    return FutureBuilder(
+      future: _getAudioDuration(),
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.none:
+            return Text('No Connection...');
+          case ConnectionState.active:
+          case ConnectionState.waiting:
+            return Text('Waiting...');
+          case ConnectionState.done:
+            if (snapshot.hasError) return Text('Error: ${snapshot.error}');
+            return Text(getTimeString(snapshot.data));
+        }
+
+          // if (snapshot.connectionState == ConnectionState.done){
+          //   return Text(getTimeString(snapshot.data));
+          // }
+          // return Text('');
+      },
+    );
+  }
+
+  String getTimeString(int milliseconds) {
+    if (milliseconds == null) milliseconds = 0;
+    String minutes =
+        '${(milliseconds / 60000).floor() < 10 ? 0 : ''}${(milliseconds / 60000).floor()}';
+    String seconds =
+        '${(milliseconds / 1000).floor() % 60 < 10 ? 0 : ''}${(milliseconds / 1000).floor() % 60}';
+    return '$minutes:$seconds'; // Returns a string with the format mm:ss
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
+    return Scaffold(
+      body: SafeArea(
       child: SingleChildScrollView(
         child: Stack(
           children: < Widget > [
@@ -76,6 +188,12 @@ class _FullState extends State < Full > {
             ),
           ],
         ),
+      ),
+    ),
+    floatingActionButton: FloatingActionButton(
+        onPressed: () => audioPlayerState == PlayerState.PLAYING ? pauseAudio() : playAudio(),
+        child: Icon(audioPlayerState == PlayerState.PLAYING ? Icons.pause_rounded : Icons.play_arrow_rounded),
+        backgroundColor: Colors.green,
       ),
     );
   }
